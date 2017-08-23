d2f79a06289f6afcb4b442e1e910c072c2ff4d62
sara heymann <saraheymann@saras-mbp.attlocal.net>
2017-08-23 09:37:37 -0500

    created server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+
+var app = express();
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(express.static(process.cwd() + '/public'));
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+var router = require('./controllers/publicArt_controller.js');
+app.use('/', router);
+
+app.listen(app.get('port'), function(){
+    console.log('running on 3000')
+});
