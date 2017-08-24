d234fa974b71583824d12faa1990123f439ea6b7
intaglioplate312 <helenelizabeth@me.com>
2017-08-24 16:11:08 -0500

    updated schema and seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,37 +1,16 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4541
-#
-# http://www.sequelpro.com/
-# https://github.com/sequelpro/sequelpro
-#
-# Host: 127.0.0.1 (MySQL 5.5.15)
-# Database: chicago_public
-# Generation Time: 2017-08-23 18:25:20 +0000
-# ************************************************************
 
-
-# Seed of table art
-# ------------------------------------------------------------
-
-
-INSERT INTO `art` (`id`, `work`, `artist`, `media`, `status`, `geolat`, `geolng`, `img`, `path`, `date`, `user_id`)
+INSERT INTO `api` (`id`, `key`, `owner`, `url`)
 VALUES
-	(1,'John Peter Altgeld','Gutzon Borglum','Sculpture',1,41.932850,-87.637337,'','public/assets/img/artworks/6.jpg','2017-08-23 13:22:28',1),
-	(3,'Untitled','Murakami','Painting',0,41.896908,-87.620232,'','public/assets/img/artworks/1.jpg','2017-08-23 13:18:48',1),
-	(4,'Untitled','unkown','Mixed',0,41.930370,87.636658,'','public/assets/img/artworks/2.jpg','2017-08-23 13:23:07',1),
-	(5,'Steel Roots','Steve Tobin','Sculpture',0,41.926498,-87.635063,'','public/assets/img/artworks/3.jpg','2017-08-23 13:23:55',1),
-	(6,'Bison','Chris Williams','Sculpture',0,41.926723,-87.635101,'','public/assets/img/artworks/4.jpg','2017-08-23 13:24:08',1),
-	(7,'Untitled','unkown','Mixed',0,41.930370,87.636658,'','public/assets/img/artworks/7.jpg','2017-08-23 13:24:10',1);
+	(1,'b19cf4c559204e82bb7d2ff12172bacf','Helen','https://spotify.com');
 
 
+INSERT INTO `art` (`id`, `title`, `artist`, `media`, `status`, `geolat`, `geolng`, `path`, `date`, `user_id`, `location`, `address`, `desciption`)
+VALUES
+	(1,'untitled','Murakami','Mural',0,NULL,NULL,'public/assets/img/artworks/1.jpg',NULL,NULL,X'7B226B31223A202276616C7565222C20226B32223A2031307D','300 E Chicago Avenue',NULL),
+	(2,'untitled','unkown','Mixed',0,NULL,NULL,'public/assets/img/artworks/2.jpg',NULL,NULL,X'7B226B31223A202276616C7565222C20226B32223A2031307D','2800 Diversey Parkway',NULL),
+	(3,'Steel Roots','Steve Tobin','Sculpture',0,NULL,NULL,'public/assets/img/artworks/3.jpg',NULL,NULL,X'7B226B31223A202276616C7565222C20226B32223A2031307D','1900 Fullerton Parkway',NULL);
 
-# Seed of table user
-# ------------------------------------------------------------
-
-
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
 
 INSERT INTO `user` (`id`, `name`, `email`, `password`, `login_name`)
 VALUES
-	(1,'Helen Smith','intagliopplate','crashtestdummy','crashtestdummy');
+	(1,'Helen Smith','intaglioplate','crashtestdummy','crashtestdummy');
