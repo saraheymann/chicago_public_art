fa463b8bb917ec0ff72c13ee675f52b364c6886e
intaglioplate312 <helenelizabeth@me.com>
2017-09-04 20:35:07 -0500

    created chain link tile for about background, cleaned up schema and seeds db files

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,15 +2,17 @@
 
 INSERT INTO `api` (`id`, `key`, `owner`, `url`)
 VALUES
-	(1,'b19cf4c559204e82bb7d2ff12172bacf','Helen','https://spotify.com'),
-	(2,'AIzaSyCf-3z1vv2pgDOcWmZnH6yKB0ZV06eLfCQ','Helen','https://www.google.com/maps');
+	(1,'n21cf4c559204e82bb7d2ff12172bacf','Helen','https://spotify.com'),
+	(2,'AIxsTyCf-3z1vv2pgDOcWmZnH6yKB0ZV06eLfCQ','Helen','https://www.google.com/maps');
 
 
-INSERT INTO `art` (`id`, `title`, `artist`, `media`, `description`, `viewable`, `img_file_path`, `geolat`, `geolng`, `google_geo`, `street_number`, `route`, `city`, `state`, `zip`, `user_id`, `date`)
+	INSERT INTO `art` (`id`, `title`, `artist`, `media`, `description`, `viewable`, `path`, `lat`, `long`, `location`, `user_id`, `date`)
 VALUES
-	(1,'untitled','Murakami','Mural','happy faces on garage on south side of building',0,'public/assets/img/artworks/1.jpg',41.895676,-87.621887,X'7B226C6174223A2034312E383935363737332C20226C6E67223A2038372E363231383835317D',220,'E Chicago Avenue','Chicago','IL',60611,NULL,NULL),
-	(2,'untitled','unkown','Mixed','semi nude image, paper on stone',0,'public/assets/img/artworks/2.jpg',41.930141,87.637466,X'7B226C6174223A2034312E393330313432372C20226C6E67223A202D38372E363337343636337D',NULL,NULL,'Chicago','IL',60614,NULL,NULL),
-	(3,'Steel Roots','Steve Tobin','Sculpture','large sculpture of recycled metal',0,'public/assets/img/artworks/3.jpg',41.926403,-87.634583,X'7B226C6174223A2034312E393236343031392C20226C6E67223A202D38372E3633343537397D',230,'N Cannon Drive','Chicago','IL',60614,NULL,NULL);
+	(1,'untitled','Murakami','Mural','happy faces on garage on south side of building',0,'/assets/img/artworks/1.jpg',000000000000041.895676,-87.621887,'{\"lat\": 41.8956773, \"lng\": 87.6218851}',NULL,NULL),
+	(2,'untitled','unkown','Mixed','semi nude image, paper on stone',0,'/assets/img/artworks/2.jpg',000000000000041.930141,-87.637466,'{\"lat\": 41.9301427, \"lng\": -87.6374663}',NULL,NULL),
+	(3,'Steel Roots','Steve Tobin','Sculpture','large sculpture of recycled metal',0,'/assets/img/artworks/3.jpg',000000000000041.926403,-87.634583,'{\"lat\": 41.9264019, \"lng\": -87.634579}',NULL,NULL),
+	(4,'untitled','unkown',NULL,'cat',0,'/assets/img/Animals.jpg',0000041.95480999999999,-87.651029,'',NULL,'2017-08-28 14:20:23'),
+	(5,'cat','unknown',NULL,'coloring page',0,'/assets/img/Animals.jpg',00000000000029.9946987,-90.1647455,'',NULL,'2017-08-29 07:22:38');
 
 
 INSERT INTO `user` (`id`, `name`, `email`, `password`, `login_name`, `timestamp`)
